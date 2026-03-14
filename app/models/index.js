3be867995e71be3e977afc69e064d331ec6e60a9
FaisyaAnindya <faisyaanindyashafah@gmail.com>
2026-03-14 12:01:47 +0700

    Final Clean: Light version with CI/CD

---
+++ app/models/index.js
@@ -0,0 +1,22 @@
+const dbConfig = require("../config/db.config.js");
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: "mysql",
+  operatorsAliases: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+
+module.exports = db;
---
+++ test/test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Sistem Check', () => {
+  it('Aplikasi harus berjalan normal', () => {
+    expect(true).to.be.true;
+  });
+});
